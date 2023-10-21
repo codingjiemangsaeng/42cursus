b6fc43c24d116b57ca5789d273564b76794943cd
Jihyun Jeong <jihyjeon@cx2r1s1.42seoul.kr>
2023-10-21 18:24:35 +0900

    list partially done

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihyjeon <jihyjeon@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 17:51:04 by jihyjeon          #+#    #+#             */
+/*   Updated: 2023/10/21 17:51:58 by jihyjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	new->content = content;
+	return (new);
+}
+
