984f7713ba90349554e20e4d324e9855dd153d99
Jihyun Jeong <jihyjeon@cx1r2s1.42seoul.kr>
2023-11-04 16:37:11 +0900

    lstsize followup

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihyjeon <jihyjeon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jihyjeon < jihyjeon@student.42seoul.kr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:51:37 by jihyjeon          #+#    #+#             */
-/*   Updated: 2023/10/26 21:04:50 by jihyjeon         ###   ########.fr       */
+/*   Updated: 2023/11/04 16:11:49 by jihyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft_bonus.h"
+#include "libft.h"
 
 int	ft_lstsize(t_list *lst)
 {
